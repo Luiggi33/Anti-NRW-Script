8e921aa8c67bc3362a37cbcf573d217d941e421a
Aron <luiggi33@web.de>
2023-07-19 10:06:35 +0200

    Change hook to PlayerConnect
    This hook is called even earlier and provides the IP aswell.

--- lua/autorun/server/sv_anti-nrw.lua
+++ lua/autorun/server/sv_anti-nrw.lua
@@ -1,5 +1,5 @@
-hook.Add("PlayerInitialSpawn", "NRW_Verboten", function(ply)
-    http.Fetch("http://ip-api.com/json/" .. ply:IPAddress(), function(body)
+hook.Add("PlayerConnect", "NRW_Verboten", function(_, ip)
+    http.Fetch("http://ip-api.com/json/" .. ip, function(body)
         local data = util.JSONToTable(body)
         if data.region == "NW" then
             ply:Ban(0, "Du kommst aus NRW, du darfst hier nicht spielen!")
