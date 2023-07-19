4264b8d6ca8f1fa27913249467ee8584e433e1dc
Aron <luiggi33@web.de>
2023-07-19 10:16:26 +0200

    Implement config

--- lua/autorun/server/sv_anti-nrw.lua
+++ lua/autorun/server/sv_anti-nrw.lua
@@ -1,8 +1,14 @@
+ANTI_NRW = ANTI_NRW or {}
+
 hook.Add("PlayerConnect", "NRW_Verboten", function(_, ip)
     http.Fetch("http://ip-api.com/json/" .. ip, function(body)
         local data = util.JSONToTable(body)
         if data.region == "NW" then
-            ply:Kick("Du kommst aus NRW, du darfst hier nicht spielen!")
+            if ANTI_NRW.Ban then
+				ply:Ban(ANTI_NRW.BanDauer, ANTI_NRW.Nachricht)
+            else
+                ply:Kick(ANTI_NRW.Nachricht)
+            end
         end
     end, function(err)
         print(err)
