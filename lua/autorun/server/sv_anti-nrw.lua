049307b7c67b1f89f512f27581adf2be22ecdfd5
Aron <luiggi33@web.de>
2023-07-19 10:07:28 +0200

    Change ban to kick
    This still works like a ban, but if you ever want to disable this, you don't need to clear your ban list anymore

--- lua/autorun/server/sv_anti-nrw.lua
+++ lua/autorun/server/sv_anti-nrw.lua
@@ -2,7 +2,7 @@ hook.Add("PlayerConnect", "NRW_Verboten", function(_, ip)
     http.Fetch("http://ip-api.com/json/" .. ip, function(body)
         local data = util.JSONToTable(body)
         if data.region == "NW" then
-            ply:Ban(0, "Du kommst aus NRW, du darfst hier nicht spielen!")
+            ply:Kick("Du kommst aus NRW, du darfst hier nicht spielen!")
         end
     end, function(err)
         print(err)
