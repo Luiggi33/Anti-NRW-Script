80bde335c8d725cb0179a98dcf962837766a70fb
Aron <luiggi33@web.de>
2023-07-29 12:36:34 +0200

    [AntiNRW] Make it work

--- lua/autorun/server/sv_anti-nrw.lua
+++ lua/autorun/server/sv_anti-nrw.lua
@@ -1,16 +1,21 @@
 ANTI_NRW = ANTI_NRW or {}
+ANTI_NRW.ToPunishPlayers = ANTI_NRW.ToPunishPlayers or {}
 
-hook.Add("PlayerConnect", "NRW_Verboten", function(_, ip)
+hook.Add("PlayerConnect", "NRW_Verboten_Handler", function(_, ip)
     http.Fetch("http://ip-api.com/json/" .. ip, function(body)
         local data = util.JSONToTable(body)
-        if data.region == "NW" then
-            if ANTI_NRW.Ban then
-				ply:Ban(ANTI_NRW.BanDauer, ANTI_NRW.Nachricht)
-            else
-                ply:Kick(ANTI_NRW.Nachricht)
-            end
+        if data.status == "success" and data.country == "Germany" and data.region == "NW" then
+            ANTI_NRW.ToPunishPlayers[ip] = true
         end
     end, function(err)
         print(err)
     end)
+end)
+
+gameevent.Listen("player_connect")
+hook.Add("player_connect", "NRW_Verboten_Punisher", function(data)
+	if ANTI_NRW.ToPunishPlayers[data.address] then
+		game.KickID(data.userid, ANTI_NRW.Nachricht)
+		ANTI_NRW.ToPunishPlayers[data.address] = nil
+	end
 end)
