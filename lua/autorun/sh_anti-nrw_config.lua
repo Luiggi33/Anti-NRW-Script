ca99d44ae0babdd20db2446b39326dca755170eb
Aron <luiggi33@web.de>
2023-07-29 12:36:52 +0200

    [AntiNRW] Removed useless config vars

--- lua/autorun/sh_anti-nrw_config.lua
+++ lua/autorun/sh_anti-nrw_config.lua
@@ -1,5 +1,3 @@
 ANTI_NRW = ANTI_NRW or {}
 
-ANTI_NRW.Ban = false -- wenn false, dann wird der Spieler nur gekickt
-ANTI_NRW.BanDauer = 0 -- in Minuten, 0 für unendlich
 ANTI_NRW.Nachricht = "Du bist aus NRW, deswegen darfst du hier leider nicht spielen." -- Die Nachricht im Ban/Kick die gezeigt wird
