80f19fb80bcf4ef8222eb4c7556dd01b6a449d73
Aron <luiggi33@web.de>
2024-01-01 22:41:01 +0100

    add cache logic to save some traffic

--- lua/autorun/server/sv_anti-nrw.lua
+++ lua/autorun/server/sv_anti-nrw.lua
@@ -1,10 +1,53 @@
 ANTI_NRW = ANTI_NRW or {}
 ANTI_NRW.ToPunishPlayers = ANTI_NRW.ToPunishPlayers or {}
+ANTI_NRW.Cache = ANTI_NRW.Cache or {}
+
+local function getMinuteDifference(timestamp1, timestamp2)
+    local minuteDifference = math.floor(math.abs(timestamp2 - timestamp1) / 60)
+    return minuteDifference
+end
+
+function ANTI_NRW:CheckCache()
+    if table.IsEmpty(self.Cache) then return end
+    local checkedCache = {}
+    local curTime = os.time()
+    for cacheIP, cacheTime in pairs(self.Cache) do
+        if getMinuteDifference(curTime, cacheTime) < ANTI_NRW.Cache_Duration then
+            checkedCache[cacheIP] = cacheTime
+        end
+    end
+    self.Cache = checkedCache
+end
+
+hook.Add("Initialize", "NRW_Verboten_Loader", function()
+    if file.Exists("anti_nrw_cache.json", "DATA") then
+        ANTI_NRW.Cache = util.JSONToTable(file.Read("anti_nrw_cache.json", "DATA") or "") or {}
+        ANTI_NRW:CheckCache()
+    end
+    timer.Create("NRW_Verboten_Checker", 60 * 30, 0, function()
+        ANTI_NRW:CheckCache()
+        file.Write("anti_nrw_cache.json", util.TableToJSON(ANTI_NRW.Cache))
+    end)
+end)
+
+hook.Add("ShutDown", "NRW_Verboten_SaveCache", function()
+    ANTI_NRW:CheckCache()
+    file.Write("anti_nrw_cache.json", util.TableToJSON(ANTI_NRW.Cache))
+end)
 
 hook.Add("PlayerConnect", "NRW_Verboten_Handler", function(_, ip)
+    if ANTI_NRW.Cache[ip] then
+        if getMinuteDifference(os.time(), ANTI_NRW.Cache[ip]) < ANTI_NRW.Cache_Duration then
+            ANTI_NRW.ToPunishPlayers[ip] = true
+            return
+        else
+            ANTI_NRW.Cache[ip] = nil
+        end
+    end
     http.Fetch("http://ip-api.com/json/" .. ip, function(body)
         local data = util.JSONToTable(body)
         if data.status == "success" and data.country == "Germany" and data.region == "NW" then
+            ANTI_NRW.Cache[ip] = os.time()
             ANTI_NRW.ToPunishPlayers[ip] = true
         end
     end, function(err)
--- lua/autorun/sh_anti-nrw_config.lua
+++ lua/autorun/sh_anti-nrw_config.lua
@@ -1,3 +1,4 @@
 ANTI_NRW = ANTI_NRW or {}
 
 ANTI_NRW.Nachricht = "Du bist aus NRW, deswegen darfst du hier leider nicht spielen." -- Die Nachricht im Ban/Kick die gezeigt wird
+ANTI_NRW.Cache_Duration = 60 * 120 -- in minutes
